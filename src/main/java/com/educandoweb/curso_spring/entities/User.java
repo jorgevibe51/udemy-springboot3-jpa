1b955abd15bfd6a46dd88cace06b2b865a43bb25
Jorge Beckhauser <jorgevibe@gmail.com>
2022-11-21 18:48:16 -0300

    user entity and resource

---
+++ src/main/java/com/educandoweb/curso_spring/entities/User.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.educandoweb.curso_spring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Jorge
+ */
+public class User implements Serializable{
+    
+    private Long id;
+    private String name;
+    private String email;
+    private String telefone;
+    private String password;
+    
+    public User(){
+    }
+
+    public User(Long id, String name, String email, String telefone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.telefone = telefone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.id, other.id);
+    }
+}
---
+++ src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.educandoweb.curso_spring.resources;
+
+import com.educandoweb.curso_spring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Jorge
+ */
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com",  "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+    
+}
