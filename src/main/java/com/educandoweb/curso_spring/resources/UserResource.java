f90564f2132a604dd585404b2a04cc76470b79d2
Jorge Beckhauser <jorgevibe@gmail.com>
2022-11-22 14:16:50 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
+++ src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
@@ -5,8 +5,13 @@
 package com.educandoweb.curso_spring.resources;
 
 import com.educandoweb.curso_spring.entities.User;
+import com.educandoweb.curso_spring.services.UserService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
     
+    @Autowired
+    private UserService service;
+    
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com",  "999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        //User u = new User(1L, "Maria", "maria@gmail.com",  "999999", "12345");
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
 }
 
 
---
+++ src/main/java/com/educandoweb/curso_spring/services/UserService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.educandoweb.curso_spring.services;
+
+import com.educandoweb.curso_spring.entities.User;
+import com.educandoweb.curso_spring.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jorge
+ */
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+    
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    
+    public Optional<User> findById(Long id){
+        return repository.findById(id);
+    }
+}
