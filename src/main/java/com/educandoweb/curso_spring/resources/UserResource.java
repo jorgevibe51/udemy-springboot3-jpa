5141c61366a06de1eefc8d9bbb4d834de29f9bf2
Jorge Beckhauser <jorgevibe@gmail.com>
2022-11-23 11:33:25 -0300

    User delete

--- src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
+++ src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,12 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User user){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(user));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
 
--- src/main/java/com/educandoweb/curso_spring/services/UserService.java
+++ src/main/java/com/educandoweb/curso_spring/services/UserService.java
@@ -32,4 +32,8 @@ public class UserService {
     public User insert(User user){
         return repository.save(user);
     }
+    
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
