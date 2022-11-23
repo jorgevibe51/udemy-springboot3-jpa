a2b0e562cc49878de43859afdcc208cfd332e8f2
Jorge Beckhauser <jorgevibe@gmail.com>
2022-11-23 11:18:23 -0300

    User insert

--- src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
+++ src/main/java/com/educandoweb/curso_spring/resources/UserResource.java
@@ -9,9 +9,12 @@ import com.educandoweb.curso_spring.services.UserService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +40,11 @@ public class UserResource {
     @GetMapping("/{id}")
     public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(service.findById(id));
+    }
+    
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(user));
     }
 }
 
